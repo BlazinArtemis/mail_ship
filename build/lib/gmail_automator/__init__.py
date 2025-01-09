32d0db563a7666081e82170270141915cfc56c7c
BlazinArtemis <oluwaseyi.ajadi@stu.cu.edu.ng>
2025-01-09 18:02:52 +0100

    Final Push

--- build/lib/gmail_automator/__init__.py
+++
@@ -1,7 +0,0 @@
-# src/gmail_automator/__init__.py
-
-from .operations import GmailAutomator
-from .email_parser import EmailParserError
-from .email_listener import EmailListenerError
-
-__all__ = ['GmailAutomator', 'EmailParserError', 'EmailListenerError']
--- build/lib/mailship/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-from .operations import GmailAutomator
-from .email_parser import EmailParserError
-from .email_listener import EmailListenerError
-from .setup_auth import set_persistent_env_var, main  # Add other required functions if needed
-
-
-__all__ = ['GmailAutomator', 'EmailParserError', 'EmailListenerError']
